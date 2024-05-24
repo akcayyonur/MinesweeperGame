9839445d2c2e7eb4627e320d3d79772f8f0b14d6
akcayyonur <167646813+akcayyonur@users.noreply.github.com>
2024-05-24 18:36:31 +0300

    Update main_screen.dart
    implementation of flags and helper

--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -1,19 +1,5 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:miinesweeper/utils/game_helper.dart';
-import '../theme/colors.dart';
-
-class MainScreen extends StatefulWidget {
-  const MainScreen({Key? key}) : super(key: key);
-
-  @override
-  _MainScreenState createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  MinesweeperGame game = MinesweeperGame();
-
-  @override
-  void initState() {import 'package:flutter/material.dart';
 import 'package:miinesweeper/utils/game_helper.dart';
 import '../theme/colors.dart';
 
@@ -28,12 +14,46 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   late MinesweeperGameHelper gameHelper;
+  Stopwatch _stopwatch = Stopwatch();
+  Timer? _timer;
 
   @override
   void initState() {
     super.initState();
     gameHelper = widget.gameHelper;
     gameHelper.generateMap();
+    _startStopwatch();
+  }
+
+  void _startStopwatch() {
+    _stopwatch.start();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {});
+    });
+  }
+
+  void _stopStopwatch() {
+    _stopwatch.stop();
+    _timer?.cancel();
+  }
+
+  String _formattedTime() {
+    final duration = _stopwatch.elapsed;
+    final minutes = duration.inMinutes.toString().padLeft(2, '0');
+    final seconds = (duration.inSeconds % 60).toString().padLeft(2, '0');
+    return '$minutes:$seconds';
+  }
+
+  void _checkWinCondition() {
+    if (gameHelper.gameWon) {
+      _stopStopwatch();
+    }
+  }
+
+  @override
+  void dispose() {
+    _stopStopwatch();
+    super.dispose();
   }
 
   @override
@@ -58,8 +78,7 @@ class _MainScreenState extends State<MainScreen> {
               Expanded(
                 child: Container(
                   margin: const EdgeInsets.symmetric(horizontal: 10.0),
-                  padding:
-                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 16.0),
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 16.0),
                   decoration: BoxDecoration(
                     color: AppColor.lightPrimaryColor,
                     borderRadius: BorderRadius.circular(8.0),
@@ -73,7 +92,7 @@ class _MainScreenState extends State<MainScreen> {
                         size: 34.0,
                       ),
                       Text(
-                        "10",
+                        "${gameHelper.remainingFlags}",
                         style: const TextStyle(
                           color: Colors.white,
                           fontSize: 32.0,
@@ -88,8 +107,7 @@ class _MainScreenState extends State<MainScreen> {
               Expanded(
                 child: Container(
                   margin: const EdgeInsets.symmetric(horizontal: 10.0),
-                  padding:
-                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 16.0),
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 16.0),
                   decoration: BoxDecoration(
                     color: AppColor.lightPrimaryColor,
                     borderRadius: BorderRadius.circular(8.0),
@@ -103,7 +121,7 @@ class _MainScreenState extends State<MainScreen> {
                         size: 34.0,
                       ),
                       Text(
-                        "10:32",
+                        _formattedTime(),
                         style: const TextStyle(
                           color: Colors.white,
                           fontSize: 32.0,
@@ -121,8 +139,8 @@ class _MainScreenState extends State<MainScreen> {
             height: 500.0,
             padding: const EdgeInsets.all(20.0),
             child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 6,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: gameHelper.columns,
                 crossAxisSpacing: 4.0,
                 mainAxisSpacing: 4.0,
               ),
@@ -137,6 +155,19 @@ class _MainScreenState extends State<MainScreen> {
                       : () {
                     setState(() {
                       gameHelper.getClickedCell(gameHelper.gameMap[index]);
+                      if (gameHelper.gameMap[index].content == "X") {
+                        gameHelper.gameOver = true;
+                        _stopStopwatch();
+                      }
+                      _checkWinCondition();
+                    });
+                  },
+                  onLongPress: gameHelper.gameOver
+                      ? null
+                      : () {
+                    setState(() {
+                      gameHelper.toggleFlag(gameHelper.gameMap[index]);
+                      _checkWinCondition();
                     });
                   },
                   child: Container(
@@ -145,7 +176,13 @@ class _MainScreenState extends State<MainScreen> {
                       borderRadius: BorderRadius.circular(6.0),
                     ),
                     child: Center(
-                      child: Text(
+                      child: gameHelper.gameMap[index].isFlagged
+                          ? Icon(
+                        Icons.flag,
+                        color: Colors.red,
+                        size: 20.0,
+                      )
+                          : Text(
                         gameHelper.gameMap[index].reveal
                             ? "${gameHelper.gameMap[index].content}"
                             : "",
@@ -165,174 +202,9 @@ class _MainScreenState extends State<MainScreen> {
             ),
           ),
           Text(
-            gameHelper.gameOver ? "Kaybettiniz" : "",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 32.0,
-            ),
-          ),
-          SizedBox(height: 20.0),
-          RawMaterialButton(
-            onPressed: () {
-              setState(() {
-                gameHelper.resetGame();
-              });
-            },
-            fillColor: AppColor.lightPrimaryColor,
-            elevation: 0,
-            shape: const StadiumBorder(),
-            padding: const EdgeInsets.symmetric(horizontal: 64.0, vertical: 24.0),
-            child: const Text(
-              "Tekrar Deneyin",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          SizedBox(height: 20.0),
-        ],
-      ),
-    );
-  }
-}
-
-
-    // TODO: implement initState
-    super.initState();
-    game.generateMap();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-//let's store the colors in a seperate file
-    return Scaffold(
-      backgroundColor: AppColor.primaryColor,
-      appBar: AppBar(
-          backgroundColor: AppColor.primaryColor,
-          elevation: 0.0,
-          centerTitle: true,
-          title: Text("MineSweeper"),
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.settings))
-          ]), // AppBar
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Expanded(
-                child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10.0),
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 16.0),
-                    decoration: BoxDecoration(
-                      color: AppColor.lightPrimaryColor,
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(
-                          Icons.flag,
-                          color: AppColor.accentColor,
-                          size: 34.0,
-                        ),
-                        Text(
-                          "10",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 32.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
-                    )),
-              ),
-              SizedBox(width: 10.0),
-              // İki konteyner arasında boşluk bırakmak için
-              Expanded(
-                child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10.0),
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 16.0),
-                    decoration: BoxDecoration(
-                      color: AppColor.lightPrimaryColor,
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(
-                          Icons.timer,
-                          color: AppColor.accentColor,
-                          size: 34.0,
-                        ),
-                        Text(
-                          "10:32",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 32.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
-                    )),
-              ),
-            ],
-          ),
-          Container(
-            width: double.infinity,
-            height: 500.0,
-            padding: EdgeInsets.all(20.0),
-            child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: MinesweeperGame.row,
-                  crossAxisSpacing: 4.0,
-                  mainAxisSpacing: 4.0,
-                ),
-                itemCount: MinesweeperGame.cells,
-                itemBuilder: (BuildContext ctx, index) {
-                  Color cellColor = game.gameMap[index].reveal
-                      ? AppColor.clickedCard
-                      : AppColor.lightPrimaryColor;
-                  return GestureDetector(
-                    onTap: game.gameOver
-                        ? null
-                        : () {
-                            setState(() {
-                              game.getClickedCell(game.gameMap[index]);
-                            });
-                          },
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: cellColor,
-                          borderRadius: BorderRadius.circular(6.0)),
-                      child: Center(
-                        child: Text(
-                          game.gameMap[index].reveal
-                              ? "${game.gameMap[index].content}"
-                              : "",
-                          style: TextStyle(
-                            color: game.gameMap[index].reveal
-                                ? game.gameMap[index].content == "X"
-                                    ? Colors.red
-                                    : AppColor.letterColors[
-                                        game.gameMap[index].content]
-                                : Colors.transparent,
-                            fontSize: 20.0,
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                }),
-          ),
-          Text(
-            game.gameOver ? "Kaybettiniz" : "",
+            gameHelper.gameOver
+                ? (gameHelper.gameWon ? "Kazandınız!" : "Kaybettiniz")
+                : "",
             style: TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
@@ -344,8 +216,9 @@ class _MainScreenState extends State<MainScreen> {
           RawMaterialButton(
             onPressed: () {
               setState(() {
-                game.resetGame();
-                game.gameOver = false;
+                gameHelper.resetGame();
+                _stopwatch.reset();
+                _startStopwatch();
               });
             },
             fillColor: AppColor.lightPrimaryColor,
@@ -361,10 +234,10 @@ class _MainScreenState extends State<MainScreen> {
             ),
           ),
           SizedBox(
-            height:20.0,
+            height: 20.0,
           ),
         ],
       ),
-    ); // Scaffold
+    );
   }
 }
